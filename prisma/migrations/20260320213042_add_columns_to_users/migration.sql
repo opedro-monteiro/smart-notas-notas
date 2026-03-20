a54e64ee90bed92265214b069c731ca65b21b254
Pedro Monteiro <pedro.oliveira@monteirodev.com>
2026-03-20 18:30:55 -0300

    feat: update User table schema by dropping password column and adding fullName and imageUrl

---
+++ prisma/migrations/20260320213042_add_columns_to_users/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "password",
+ADD COLUMN     "fullName" TEXT,
+ADD COLUMN     "imageUrl" TEXT,
+ALTER COLUMN "username" DROP NOT NULL;
