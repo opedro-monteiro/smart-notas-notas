34b4a290cd8fbeecea4dfc0bced1542d77c18ef0
Pedro Monteiro <pedro.oliveira@monteirodev.com>
2026-03-20 17:30:23 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log("hello word");
-console.log("hello word");
+import "dotenv/config";
+
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3333 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
